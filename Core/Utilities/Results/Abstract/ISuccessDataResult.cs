2dee0883209a08113246519cfb6f5f25c243d852
Tahir1072a <tahirifdn@gmail.com>
2023-07-17 20:49:04 +0300

    Results Interfaceleri Eklendi!

---
+++ Core/Utilities/Results/Abstract/ISuccessDataResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public interface ISuccessDataResult<T> : IDataResult<T>
+    {
+    }
+}
