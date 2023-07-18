853ade042c564c2afeacafb1c898a2909ae23192
Tahir1072a <tahirifdn@gmail.com>
2023-07-18 10:49:05 +0300

    1.0.2

--- Core/Utilities/Results/Abstract/ISuccessDataResult.cs
+++ Core/Utilities/Results/Abstract/ISuccessDataResult.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Utilities.Results.Abstract
+namespace Core.Utilities.Results
 {
     public interface ISuccessDataResult<T> : IDataResult<T>
     {
--- Core/Utilities/Results/Concrete/ErrorResult.cs
+++ Core/Utilities/Results/Concrete/ErrorResult.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results;
 
-namespace Core.Utilities.Results.Concrete
+namespace Core.Utilities.Results
 {
     public class ErrorResult : IErrorResult
     {
